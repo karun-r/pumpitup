bc2585f06004d93521c928036b69f9a7dd224865
karun-r <karun_r@outlook.com>
2018-09-15 12:41:42 -0700

    initial commit with EDA file and repo structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Competition in www.drivendata.org to predict which among the installed pumps would fail in near future.',
+    author='karun-r',
+    license='MIT',
+)
